9f4ca2cbd5a9f66a4d0dd2226e08f255ec8bc8da
Remy <r.zyngfogel@calypso.science>
2020-08-27 11:59:00 +1200

    update test

--- _tests/test_transformation.py
+++ _tests/test_transformation.py
@@ -19,21 +19,24 @@ df = pd.DataFrame(arr,index=dates, columns=['Depth','U', 'V', 'Hs','Hs_sw','Tp',
 tf={}
 tf['test1']={}
 tf['test1']['dataframe']=df
-df=tf['test1']['dataframe'].DataTransformation.calc_spdir(u='U',v='V')
-print(df)
+tf['test1']['dataframe']['U'].short_name='U'
+tf['test1']['dataframe']['spd'].short_name='ws'
+
+df=tf['test1']['dataframe'].DataTransformation.uv_to_spddir(u='U',v='V')
 print('speed=%.2f and drr=%.2f' %(df['spd'][0],df['drr'][0]))
 
-df=tf['test1']['dataframe'].DataTransformation.calc_uv(spd='spd',direc='drr')
+df=tf['test1']['dataframe'].DataTransformation.spddir_to_uv(spd='spd',direc='drr')
 print('u=%.2f and v=%.2f' %(df['u'][0],df['v'][0]))
 
-sys.exit(-1)
-Hs=tf['test1']['dataframe'].trans.hs_sea(hs='Hs',hs_swell='Hs_sw')
-print('Hs sea=%.2f' %(Hs[0]))
-Uorb=tf['test1']['dataframe'].trans.Uorb(dp='Depth',tp='Tp',hs='Hs',z=0)
-print('Uorb=%.2f' %(Uorb[0]))
-ly=tf['test1']['dataframe'].trans.dav2layers(u='U',dp='Depth')
-print('U0=%.2f' %(ly[0]))
-ly=tf['test1']['dataframe'].trans.layers2dav(u='U',dp='Depth')
-print('Um=%.2f' %(ly[0]))
-ly=tf['test1']['dataframe'].trans.windprofile(ws='spd',opts={'Output level (in meters)':200})
-print('Wspd=%.2f' %(ly[0]))
+df=tf['test1']['dataframe'].DataTransformation.hs_sea(hs='Hs',hs_swell='Hs_sw')
+print('Hs sea=%.2f' %(df['Hs_sea'][0]))
+df=tf['test1']['dataframe'].DataTransformation.Oribital_velocity(dp='Depth',tp='Tp',hs='Hs',args={'z':0})
+print('Uorb=%.2f' %(df['Uorb'][0]))
+
+df=tf['test1']['dataframe'].DataTransformation.dav_to_layers(u='U',dp='Depth')
+print(df)
+df=tf['test1']['dataframe'].DataTransformation.layers_to_dav(u='U',dp='Depth')
+print(df)
+
+df=tf['test1']['dataframe'].DataTransformation.wind_profile(ws='spd',args={'Output level (in meters)':200})
+print('Wspd=%.2f' %(df['Uz'][0]))
