c2a0c079bffdc7d10b16fe6a22d22f35f02d0bc0
Remy Zyngfogel <remy@system76-pc.localdomain>
2021-06-01 22:40:49 +1200

    add .WAVE format

--- toto/inputs/tryaxis.py
+++ toto/inputs/tryaxis.py
@@ -12,7 +12,7 @@ class TRYAXISfile():
 
     @staticmethod
     def defaultExtensions():
-        return ['.NONDIRSPEC','.DIRSPEC']
+        return ['.NONDIRSPEC','.WAVE']
 
 
     def __init__(self,filenames):
@@ -28,8 +28,65 @@ class TRYAXISfile():
         if self.filenames[0].endswith('NONDIRSPEC'):
             self._reads_NONDIRSPEC()
 
+        # READ NONIRCSPCE
+        if self.filenames[0].endswith('WAVE'):
+            self._reads_WAVE()
+
+    def _read_WAVE(filename):
+        # Using readline()
+        wave={}
+        file1 = open(filename, 'r')
+        count = 0
+         
+        while True:
+            count += 1
+            # Get next line from file
+            line = file1.readline()
+            # if line is empty
+            # end of file is reached
+            if not line:
+                break
+
+            if count>3:
+                name,value=line.split('=')
+                if count==4:
+                    value=pd.to_datetime(value.rstrip(), format=' %Y-%m-%d %H:%M(UTC)')
+                elif count>7:
+                    try:
+                        value=float(value)
+                    except:
+                        value=np.nan
+                else:
+                    value=value.rstrip()
+                wave[name.rstrip().replace(' ','_')]=value
+
+        file1.close()
+        return wave
+    def _reads_WAVE(self):
+        ds=[]
+        for i,file in enumerate(self.filenames):
+            ds.append(TRYAXISfile._read_WAVE(file))
+
+        keys=[]
+        for d in ds:
+            keys+=d.keys()
+        keys=list(set(keys))
+
+        di = {}
+        for key in keys:
+            di[key]=[]
+            for d in ds:
+                if key in d:
+                    di[key].append(d[key])
+                else:
+                    di[key].append(np.nan)
 
 
+        df=pd.DataFrame.from_dict(di,orient='columns')
+        df=df.rename(columns={'DATE':'time'})
+        df.set_index('time',inplace=True,drop=False)
+        df.sort_index(inplace=True)
+        self.data.append(df)
     def _reads_NONDIRSPEC(self):
         for file in self.filenames:
             self._read_NONDIRSPEC(file)
