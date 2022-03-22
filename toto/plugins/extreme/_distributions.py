12bd33841dbb07b648c29de59bb9e5943b896f9d
Remy Zyngfogel <remy@system76-pc.localdomain>
2022-03-23 08:50:13 +1300

    doc try

--- toto/plugins/extreme/_distributions.py
+++ toto/plugins/extreme/_distributions.py
@@ -8,11 +8,9 @@
 from copy import copy
 from functools import partial
 
-try:
-    from ._infra import entropy, rv_discrete, rv_continuous, rv_frozen
-except:
-    print('could not load totaly....')
-    
+from ._infra import entropy, rv_discrete, rv_continuous, rv_frozen
+
+
 from scipy.stats import _continuous_distns
 from scipy.stats import _discrete_distns
 from scipy.stats._constants import _EULER
