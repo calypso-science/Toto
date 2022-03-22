5ae327605243d4a51697ca53282c0f1600fc5243
Remy Zyngfogel <remy@system76-pc.localdomain>
2022-03-23 08:36:25 +1300

    try for online doc

--- toto/plugins/extreme/_distributions.py
+++ toto/plugins/extreme/_distributions.py
@@ -7,8 +7,12 @@
 #
 from copy import copy
 from functools import partial
-from ._infra import entropy, rv_discrete, rv_continuous, rv_frozen
 
+try:
+    from ._infra import entropy, rv_discrete, rv_continuous, rv_frozen
+except:
+    print('could not load totaly....')
+    
 from scipy.stats import _continuous_distns
 from scipy.stats import _discrete_distns
 from scipy.stats._constants import _EULER
